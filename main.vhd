b6dcafdfbfeba25d413f37c5d2ae5ebb8b1a3a88
Lucas Vieira <lucasvieira94@poli.ufrj.br>
2018-10-05 21:24:15 -0300

    Fixed logic error on displaying results and operands in main file, now they have correct logic to run on the board

--- main.vhd
+++ main.vhd
@@ -33,8 +33,9 @@ entity main is
     port ( clock : in  STD_LOGIC;
            oper : in  STD_LOGIC_VECTOR (2 downto 0);
            output : out  STD_LOGIC_VECTOR (3 downto 0);
-           selector : out  STD_LOGIC_VECTOR (1 downto 0);
-			  flags : out STD_LOGIC_VECTOR (3 downto 0));
+			  --resultado : inout  STD_LOGIC_VECTOR (3 downto 0);
+           selector : inout  STD_LOGIC_VECTOR (1 downto 0));
+			  --flags : inout STD_LOGIC_VECTOR (3 downto 0));
 end main;
 
 architecture Behavioral of main is
@@ -48,9 +49,13 @@ signal resultadoSoma: STD_LOGIC_VECTOR(3 downto 0);
 signal resultadoSub: STD_LOGIC_VECTOR(3 downto 0);
 signal seletorInterno : STD_LOGIC_VECTOR(1 downto 0);
 signal carryOut : STD_LOGIC;
+signal carryBorrow : STD_LOGIC;
+signal borrowOut : STD_LOGIC;
 signal zero : STD_LOGIC;
 signal negativo : STD_LOGIC;
 signal overflow : STD_LOGIC;
+signal flags : STD_LOGIC_VECTOR (3 downto 0);
+signal resultado : STD_LOGIC_VECTOR (3 downto 0);
 
 component fullAdder4bits
 port ( 	  a : in  STD_LOGIC_VECTOR (3 downto 0);
@@ -72,17 +77,16 @@ port ( 	  clock : in  STD_LOGIC;
 end component contador10bits;
 
 begin
-	
-	seletorInterno(0) <= counter(0);
-	seletorInterno(1) <= counter(1);
-	A(0) <= counter(2);
-	A(1) <= counter(3);
-	A(2) <= counter(4);
-	A(3) <= counter(5);
-	B(0) <= counter(6);
-	B(1) <= counter(7);
-	B(2) <= counter(8);
-	B(3) <= counter(9);
+		seletorInterno(0) <= counter(0);
+		seletorInterno(1) <= counter(1);
+		A(0) <= counter(2);
+		A(1) <= counter(3);
+		A(2) <= counter(4);
+		A(3) <= counter(5);
+		B(0) <= counter(6);
+		B(1) <= counter(7);
+		B(2) <= counter(8);
+		B(3) <= counter(9);
 
 	c0: redutor port map (clock, clockReduzido);
 	c1: contador10bits port map (clockReduzido, counter);
@@ -91,12 +95,12 @@ begin
 	cB <= B WHEN (B = "0000")
 	ELSE NOT(B);
 	
-	SUB: fullAdder4bits port map(A, cB, '1', resultadoSub, carryOut);
+	SUB: fullAdder4bits port map(A, cB, '1', resultadoSub, borrowOut);
 	
-	upcount: PROCESS(clock) 
-	BEGIN
+	--upcount: PROCESS(clock) 
+	--BEGIN
 	
-		output <= resultadoSoma WHEN (oper = "000")
+		resultado <= resultadoSoma WHEN (oper = "000")
 			  ELSE resultadoSub WHEN (oper = "001")
 			  ELSE (A AND B) WHEN (oper = "010")
 			  ELSE (A OR B) WHEN (oper = "011")
@@ -105,18 +109,29 @@ begin
 			  ELSE (A XNOR B) WHEN (oper = "110")
 			  ELSE (A NAND B);
 			  
-		carryOut <= NOT(carryOut) WHEN (oper = "001")
+		carryBorrow <= NOT(borrowOut) WHEN (oper = "001")
 			ELSE carryOut;		
 			  
-		zero <= "1" WHEN (output = "0000")
-			ELSE "0";
+		zero <= '1' WHEN (resultado = "0000")
+			ELSE '0';
 			
-		negativo <= "1" WHEN (output(3) = "1")
-			ELSE "0";
+		negativo <= '1' WHEN (resultado(3) = '1')
+			ELSE '0';
 		
-		overflow <= "1" WHEN ((A(3) = B(3)) AND (output(3) = NOT(A(3))))
-			ELSE "0";
+		overflow <= '1' WHEN ((A(3) = B(3)) AND (resultado(3) = NOT(A(3))))
+			ELSE '0';
+			
+		flags(0) <= carryBorrow;
+		flags(1) <= zero;
+		flags(2) <= negativo;
+		flags(3) <= overflow;
 		
-	END PROCESS upcount;	
+		output <= A WHEN (seletorInterno = "00")
+			else B WHEN(seletorInterno = "01")
+			else resultado WHEN(seletorInterno = "10")
+			else flags;
+		
+		selector <= seletorInterno;
+		
+	--END PROCESS upcount;	
 end Behavioral;
-
